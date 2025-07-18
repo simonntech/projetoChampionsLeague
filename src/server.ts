d264a76ea084f74da8a8d8fc0f29a6667417acda
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-18 16:06:18 -0300

    routes e controller

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import express, {json, Request, Response} from "express";
+import createApp from "./app";
 
-const app = express();
+const app = createApp();
 const port = process.env.PORT;
-
-//midleware para configurar requisição JSON
-app.use(json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ player: "Tsubasa"});
-});
 
 app.listen(port, () => {
     console.log(`💜 Server running at http://localhost:${port}`);
